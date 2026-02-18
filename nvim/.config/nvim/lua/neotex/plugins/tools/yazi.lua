ff5c1bc6535ed189dd4b2b1a1d6f37c0dcf98408
Malik Hacini <juioiplm@gmail.com>
2026-02-18 15:52:00 +0100

    nvim: enable yazi to change cwd
    change nvim cwd by pressing "enter" on a directory in yazi

--- nvim/.config/nvim/lua/neotex/plugins/tools/yazi.lua
+++ nvim/.config/nvim/lua/neotex/plugins/tools/yazi.lua
@@ -4,6 +4,15 @@ return {
   -- Keybinds are in which-key.lua
   opts = {
     open_for_directories = false,
+    open_file_function = function(chosen_file)
+      if vim.fn.isdirectory(chosen_file) == 1 then
+        vim.cmd({ cmd = "cd", args = { chosen_file } })
+        vim.notify('cwd changed to "' .. chosen_file .. '"')
+        return
+      end
+
+      require("yazi.openers").open_file(chosen_file)
+    end,
     keymaps = {
       show_help = '<f1>',
     },
