ae635ec95f2c720b8522a279f72cd349f7d5848e
Malik Hacini <juioiplm@gmail.com>
2026-04-13 13:07:46 +0200

    fix yazi url

--- install/tools.sh
+++ install/tools.sh
@@ -585,7 +585,7 @@ install_yazi() {
             ;;
     esac
 
-ases/download/${YAZI_VERSION}/${asset}"
+    YAZI_URL="https://github.com/sxyazi/yazi/releases/download/${YAZI_VERSION}/${asset}"
     local TMP_DIR
     TMP_DIR=$(mktemp -d)
 
