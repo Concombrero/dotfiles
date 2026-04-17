23fe3cd9b78b854cf818cc3c74722bf9c8054fe2
Malik Hacini <juioiplm@gmail.com>
2026-04-17 14:47:07 +0200

    nvim: fix typst icon

--- nvim/.config/nvim/lua/plugins/ui/nvim-web-devicons.lua
+++ nvim/.config/nvim/lua/plugins/ui/nvim-web-devicons.lua
@@ -1,5 +1,20 @@
 return {
   "nvim-tree/nvim-web-devicons",
   lazy = true,
-  opts = {},
+  opts = {
+    -- This nvim-web-devicons version does not ship a Typst icon.
+    override_by_extension = {
+      typ = {
+        icon = "",
+        color = "#0DBCC0",
+        cterm_color = "37",
+        name = "Typst",
+      },
+    },
+  },
+  config = function(_, opts)
+    local devicons = require("nvim-web-devicons")
+    devicons.setup(opts)
+    devicons.set_icon_by_filetype({ typst = "typ" })
+  end,
 }
